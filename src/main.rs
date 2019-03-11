85ec3e09d09bc74a8103fbe46d758ec97e1b9732
reisub0 <reisub0@gmail.com>
2019-03-11 12:16:03 +0530

    Working command line arguments
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,35 @@
 //
 
 #![feature(type_ascription)]
-#[macro_use]
-extern crate serde_derive;
 extern crate dirs;
 extern crate docopt;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate prettytable;
 
 mod args;
 mod nzb;
 
 pub fn main() -> Result<(), Box<std::error::Error>> {
-    let args = args::parse_args();
-    println!("{:?}", nzb::get_inbox()?);
+    let a = args::parse_args();
+    // println!("{:?}", a);
+    if let Some(s) = a.flag_auth {
+        unsafe {
+            nzb::TOKEN = Box::leak(Box::new(s));
+        }
+    }
+    if let Some(x) = a.arg_command {
+        match x {
+            args::Command::All => nzb::print_all()?,
+            args::Command::Conky => unimplemented!(),
+            args::Command::Inbox => nzb::print_inbox()?,
+            args::Command::Next => unimplemented!(),
+        }
+    } else {
+        // Default action = Print all
+        nzb::print_all()?;
+        // unimplemented!();
+    }
     Ok(())
 }
