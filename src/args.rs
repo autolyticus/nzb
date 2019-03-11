49cea8c5f63bc8e9025c566d1e022b3f94e8c79e
reisub0 <reisub0@gmail.com>
2019-03-11 12:14:50 +0530

    Standardised CLI, working authentication token
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- src/args.rs
+++ src/args.rs
@@ -10,34 +10,42 @@ const USAGE: &'static str = "
 Nozbe front-end written in Rust.
 
 Usage:
-  nzb
-  nzb all
-  nzb inbox
-  nzb next
-  nzb (-h | --help)
+  nzb [options] [<command> [<args>...]]
+  nzb -h | --help
   nzb --version
 
 Options:
-  --auth <token>   Specify the Nozbe authentication token (Refer Nozbe API Documentation)
-  -h --help        Show this screen.
-  --version        Show version.
+  -a <token> --auth=<token>    Specify the Nozbe authentication token (Refer Nozbe API Documentation)
+  -h --help                   Show this screen.
+  -V --version                    Show version.
 
 Commands:
-  all              View all your tasks
-  conky            A conky-friendly, colourful summary of all your tasks
-  inbox            View your inbox
-
+  all                          View all your tasks (This is the default action)
+  conky                        A conky-friendly, colourful summary of all your tasks
+  inbox                        View your inbox
+  next                         View next(starred) tasks
 ";
 
 #[derive(Debug, Deserialize)]
 pub struct Args {
-    cmd_inbox: bool,
-    cmd_all: bool,
-    cmd_next: bool,
+    pub arg_command: Option<Command>,
+    pub arg_args: Vec<String>,
+    pub flag_auth: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub enum Command {
+    All,
+    Conky,
+    Inbox,
+    Next,
 }
 
 pub fn parse_args() -> Args {
     Docopt::new(USAGE)
-        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit())
+        .options_first(true)
+        .version(Some("Nzb: A Nozbe client (v0.1.1)".to_owned()))
+        .deserialize()
         .unwrap_or_else(|e| e.exit())
 }
