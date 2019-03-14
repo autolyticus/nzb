9d1484537fdedb8def194ff93d2d840b76b50f51
reisub0 <reisub0@gmail.com>
2019-03-14 20:48:55 +0530

    Adds FZF-like UI for marking tasks
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- src/done.rs
+++ src/done.rs
@@ -4,6 +4,33 @@
 // Distributed under terms of the MIT license.
 //
 
+use super::nzb::{get_tasks, mark_done};
+use std::io::Cursor;
+
 pub fn done(args: Vec<String>) -> Result<(), Box<std::error::Error>> {
-    unimplemented!()
+    let query = args.join("");
+    let options = skim::SkimOptions::default()
+        .height("10%")
+        .multi(true)
+        .prompt("Select a task to mark as done (Multi-select w/ TAB) >> ")
+        .query(&query);
+    // .reverse(true);
+    let tasks = get_tasks()?;
+    let task_string = tasks
+        .clone()
+        .into_iter()
+        .map(|x| x.name)
+        .collect::<Vec<_>>()
+        .join("\n");
+    let selected_items = skim::Skim::run_with(&options, Some(Box::new(Cursor::new(task_string))))
+        .map(|out| out.selected_items)
+        .unwrap_or_default();
+    mark_done(
+        tasks,
+        selected_items
+            .into_iter()
+            .map(|x| x.get_index())
+            .collect::<Vec<_>>(),
+    )?;
+    Ok(())
 }
